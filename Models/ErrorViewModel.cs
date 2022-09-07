05f66a0907f692573609bebe5721173f1d34dd00
M3TriX <frederickvalle14@gmail.com>
2022-09-06 22:36:52 -0600

    Proyecto creado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Cuaderno_virtual.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
